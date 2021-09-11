5d51d79c0670f8a6605a236195f54a4a5330cf74
Fredrik Sigvartsen <fredrik@sealab.no>
2021-09-11 10:40:29 +0200

    Added navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { Box, Heading } from "@chakra-ui/react"
+export const Navbar = () => {
+  return (
+    <Box w="100%">
+      <Heading>Fredrik & Kamilla</Heading>
+    </Box>
+  )
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,10 +2,16 @@ import * as React from "react"
 
 // 1. import `ChakraProvider` component
 import { ChakraProvider } from "@chakra-ui/react"
+import { Navbar } from "./Navbar"
 
 const Layout = ({ children }) => {
   // 2. Use at the root of your app
-  return <ChakraProvider>{children}</ChakraProvider>
+  return (
+    <ChakraProvider>
+      <Navbar />
+      {children}
+    </ChakraProvider>
+  )
 }
 
 export default Layout
