c72f659abfaf77828743a6a39f708eacd6308784
Fredrik Sigvartsen <fredrik@sealab.no>
2021-11-12 22:28:37 +0100

    New simpler form

--- src/pages/rsvp.js
+++ src/pages/rsvp.js
@@ -5,68 +5,9 @@ import { MinusIcon } from "@chakra-ui/icons"
 import Layout from "../components/Layout"
 import { navigate } from "gatsby-link"
 
-import {
-  Box,
-  CheckboxGroup,
-  Checkbox,
-  Grid,
-  Input,
-  InputGroup,
-  Heading,
-  Button,
-  Center,
-  InputRightElement,
-} from "@chakra-ui/react"
-
-const allergies = [
-  { name: "laktose", display: "Laktose" },
-  { name: "gluten", display: "Gluten" },
-  { name: "nuts", display: "Nøtter" },
-  { name: "fisk", display: "Fisk" },
-  { name: "egg", display: "Egg" },
-  { name: "skalldyr", display: "Skalldyr" },
-]
+import { Box, Input, Text, Textarea, Heading, Center } from "@chakra-ui/react"
 
 const RSVP = () => {
-  const [someoneHasAllergies, setHasAllergies] = useState(false)
-  const [participants, setParticipants] = useState([])
-  const [formValues, setFormValues] = useState({ allergies_name: "" })
-
-  function encode(data) {
-    return Object.keys(data)
-      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&")
-  }
-
-  const handleSubmit = event => {
-    event.preventDefault()
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({
-        "form-name": event.target.getAttribute("name"),
-        participants: _.toString(participants),
-        ...formValues,
-      }),
-    })
-      .then(() => navigate("/takk/"))
-      .catch(error => alert(error))
-  }
-
-  const onParticipantChange = participants => {
-    console.log(...participants)
-    setParticipants(participants)
-  }
-
-  const handleChange = e => {
-    setFormValues({ ...formValues, [e.target.name]: e.target.value })
-  }
-
-  const onCheckChange = (name, value) => {
-    setFormValues({ ...formValues, [name]: value })
-    setHasAllergies(true)
-  }
-
   return (
     <Layout title="Kontakt">
       <Box pb="60rem" h="xl" width="100%">
@@ -75,64 +16,7 @@ const RSVP = () => {
         </Heading>
         <Center>
           <Box w="xl" mt="40px" p="20px">
-            <form
-              name="rsvp"
-              method="POST"
-              data-netlify="true"
-              data-netlify-honeypot="bot-field"
-              action="/takk/"
-              onSubmit={handleSubmit}
-            >
-              {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-              <input type="hidden" name="form-name" value="rsvp" />
-              <div hidden>
-                <label>
-                  Don’t fill this out: <input name="bot-field" />
-                </label>
-              </div>
-              <AddParticipants onChangeParticipants={onParticipantChange} />
-
-              <Heading mt="30px" mb="10px">
-                Allergier
-              </Heading>
-              <CheckboxGroup>
-                <Grid
-                  templateColumns={["repeat(auto-fit, minmax(70px,1fr))"]}
-                  gap={3}
-                  mb="20px"
-                >
-                  {allergies.map((allergy, index) => {
-                    return (
-                      <Checkbox
-                        key={index}
-                        name={allergy.name.toLowerCase()}
-                        value={allergy.name.toLowerCase()}
-                        onChange={e =>
-                          onCheckChange(allergy.name, !formValues[allergy.name])
-                        }
-                        isChecked={formValues[allergy.name] || false}
-                        checked={formValues[allergy.name] || false}
-                      >
-                        {allergy.display}
-                      </Checkbox>
-                    )
-                  })}
-                </Grid>
-              </CheckboxGroup>
-              {someoneHasAllergies ? (
-                <Input
-                  type="messsage"
-                  name="allergies_name"
-                  placeholder="Hvem av dere har allergier?"
-                  onChange={handleChange}
-                  size="sm"
-                />
-              ) : null}
-
-              <Button mt="20px" colorScheme="pink" type="submit">
-                Send inn
-              </Button>
-            </form>
+            <Form />
           </Box>
         </Center>
       </Box>
@@ -140,61 +24,31 @@ const RSVP = () => {
   )
 }
 
-const AddParticipants = ({ onChangeParticipants }) => {
-  const [participants, setParticipants] = useState([""])
-
-  useEffect(() => {
-    onChangeParticipants(participants) // eslint-disable-next-line
-  }, [participants])
-
-  const handleClick = () => {
-    setParticipants([...participants, ""])
-  }
-
-  const handleParticipantDeletion = index => {
-    let newParticipants = participants
-    newParticipants.splice(index, 1)
-    setParticipants([...newParticipants])
-  }
-
-  const handleChangeParticipantName = (event, index) => {
-    if (!event || !event.target || !event.target.value) return
-    let newParticipants = participants
-    newParticipants[index] = event.target.value
-    setParticipants([...newParticipants])
-  }
-
+const Form = () => {
   return (
-    <Box>
-      {participants.map((participant, index) => {
-        return (
-          <InputGroup key={index} mb="20px">
-            <Input
-              onChange={event => handleChangeParticipantName(event, index)}
-              value={participant}
-              placeholder="Navn"
-              type="text"
-              name={`name-${index}`}
-            ></Input>
-            <InputRightElement
-              children={
-                <IconButton
-                  onClick={() => handleParticipantDeletion(index)}
-                  size="xs"
-                  isRound
-                  bg="transparent"
-                  aria-label="Slett person"
-                  icon={<MinusIcon color="blackAlpha.300" />}
-                />
-              }
-            ></InputRightElement>
-          </InputGroup>
-        )
-      })}
-      <Button variant="link" onClick={handleClick}>
-        + Legg til nytt navn
-      </Button>
-    </Box>
+    <form
+      name="response"
+      method="post"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+      className="contact-us"
+    >
+      <input type="hidden" name="bot-field" />
+      <Text mb="5px">Hvem kommer?</Text>
+      <Textarea
+        name="participants"
+        type="text"
+        placeholder="Fyll inn navn her"
+        isRequired
+        mb={["10px", "30px"]}
+      />
+      <Text mb="5px">Allergier?</Text>
+      <Input
+        mb={["10px", "30px"]}
+        placeholder="Fyll inn allergier, og hvem som har det"
+        size="sm"
+      />
+    </form>
   )
 }
 
